a5885c6fbd6f4c3890e2c14a1aea3d13f5530417
Sivaji04 <sudula444@gmail.com>
2025-03-11 22:42:17 +0530

    updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,73 @@
 import MotionHoc from "./MotionHoc";
-
+import "./index.css"
 const HomeComponent = () => {
-  return <h1>Home</h1>;
+  return (
+  <>
+    <div className="card">
+      <div className="card1">
+        <h1>Feature 1</h1>
+        <p>sid onsf;od fdoij ifd ridjfgo dfogxbn ofsduuhb  fdgih i fxoi odfi rgdfog oih hgfxigr sodfh grjx roidj </p>
+        <button>USE TOOL</button>
+      </div>
+
+      <div className="card1">
+        <h1>Feature 1</h1>
+        <p>sid onsf;od fdoij ifd ridjfgo dfogxbn ofsduuhb  fdgih i fxoi odfi rgdfog oih hgfxigr sodfh grjx roidj </p>
+        <button>USE TOOL</button>
+      </div>
+
+      <div className="card1">
+        <h1>Feature 1</h1>
+        <p>sid onsf;od fdoij ifd ridjfgo dfogxbn ofsduuhb  fdgih i fxoi odfi rgdfog oih hgfxigr sodfh grjx roidj </p>
+        <button>USE TOOL</button>
+      </div>
+
+      <div className="card1">
+        <h1>Feature 1</h1>
+        <p>sid onsf;od fdoij ifd ridjfgo dfogxbn ofsduuhb  fdgih i fxoi odfi rgdfog oih hgfxigr sodfh grjx roidj </p>
+        <button>USE TOOL</button>
+      </div>
+
+      <div className="card1">
+        <h1>Feature 1</h1>
+        <p>sid onsf;od fdoij ifd ridjfgo dfogxbn ofsduuhb  fdgih i fxoi odfi rgdfog oih hgfxigr sodfh grjx roidj </p>
+        <button>USE TOOL</button>
+      </div>
+
+      <div className="card1">
+        <h1>Feature 1</h1>
+        <p>sid onsf;od fdoij ifd ridjfgo dfogxbn ofsduuhb  fdgih i fxoi odfi rgdfog oih hgfxigr sodfh grjx roidj </p>
+        <button>USE TOOL</button>
+      </div>
+
+      <div className="card1">
+        <h1>Feature 1</h1>
+        <p>sid onsf;od fdoij ifd ridjfgo dfogxbn ofsduuhb  fdgih i fxoi odfi rgdfog oih hgfxigr sodfh grjx roidj </p>
+        <button>USE TOOL</button>
+      </div>
+
+      <div className="card1">
+        <h1>Feature 1</h1>
+        <p>sid onsf;od fdoij ifd ridjfgo dfogxbn ofsduuhb  fdgih i fxoi odfi rgdfog oih hgfxigr sodfh grjx roidj </p>
+        <button>USE TOOL</button>
+      </div>
+
+      <div className="card1">
+        <h1>Feature 1</h1>
+        <p>sid onsf;od fdoij ifd ridjfgo dfogxbn ofsduuhb  fdgih i fxoi odfi rgdfog oih hgfxigr sodfh grjx roidj </p>
+        <button>USE TOOL</button>
+      </div>
+
+
+      <div className="card1">
+        <h1>Feature 1</h1>
+        <p>sid onsf;od fdoij ifd ridjfgo dfogxbn ofsduuhb  fdgih i fxoi odfi rgdfog oih hgfxigr sodfh grjx roidj </p>
+        <button>USE TOOL</button>
+      </div>
+
+    </div>
+  </>
+  )
 };
 
 const Home = MotionHoc(HomeComponent);
